9d9a4d7f5a40c8172b47cc2295ac7b7c5d0ade25
sara taboas <sarataboas04@gmail.com>
2026-06-21 11:12:51 +0100

    rm: remove deprecated retrieval script

--- detection_and_retrieval/retrieval/table_retrieval.py
+++
@@ -1,7 +0,0 @@
-"""
-Table retrieval: 
-    - Select the most similar pooling table from the training data
-
-    Evaluation --> Qualitative evaluation with some (good and bad) results are enough
-
-    """
